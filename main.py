6dc4afd6c02c956b15dbcd4be44ef47535bd379c
ni-lie <cjay.coronel25@gmail.com>
2024-01-30 18:10:13 +0800

    feat: get address from either cmd line or clipboard

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 
+if len(sys.argv) > 1:
+    # get address from command line
+    address = ' '.join(sys.argv[1:])
+else:
+    # get address from clipboard
+    address = pyperclip.paste()
